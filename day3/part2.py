b2742aa4242a866792b728f495c7ede857d131c5
alrokosz <alrokosz@gmail.com>
2023-12-04 18:24:49 -0700

    day 3 part 2

--- day3/part2.py
+++ day3/part2.py
@@ -1,7 +1,25 @@
+from utils import read_file_lines, are_nbrs_nums
+
 # find all stars with adjacent numbers
+def find_gear_ratio_sum(lines):
+    total = 0
+
+    for row in range(len(lines)):
+        for col in range(len(lines[row])):
+            end = len(lines[row]) - 1
+            char = lines[row][col]
+            
+            if char == '*':
+                nbrs = are_nbrs_nums(lines, col, row)
+                if len(nbrs) == 2:
+                    total += int(nbrs[0]) * int(nbrs[1])
+
+    return total
+                
 
 # find adjacent numbers
 
 # squre numbers and add to total
-
+lines = read_file_lines('day3/data.txt')
+print(find_gear_ratio_sum(lines))
 # return total
--- day3/utils.py
+++ day3/utils.py
@@ -33,4 +33,56 @@ def are_nbrs_chars(lines: list, num_start, num_end, row: int) -> bool:
     return False
 
 
+
+def find_num_nbrs(row, visited, index, r):
+    if (index < 0) or (index > len(row) - 1) or (not row[index].isdigit()) or (f'{r}{index}' in visited):
+        return ''
+    # print(f'{r}{index}')
+    visited.add(f'{r}{index}')
+    return find_num_nbrs(row, visited, index - 1, r) + row[index] + find_num_nbrs(row, visited, index + 1, r)
+
+
+def are_nbrs_nums(lines: list, index: int, row: int) -> bool:
+    visited = set()
+    nbrs = []
+
+    start = index - 1 if index > 0 else 0
+    end = index + 2 if index < len(lines[row]) - 1 else index + 1
+
+    if row > 0:
+        above = lines[row - 1][start : end]
+        for i in range(len(lines[row - 1])):
+            if start <= i < end:
+                char = lines[row - 1][i]
+                if char.isdigit():
+                    n = find_num_nbrs(lines[row - 1], visited, i, row - 1)
+                    if n != '':
+                        nbrs.append(n)
+
+    if row < len(lines) - 1:
+        below = lines[row + 1][start : end]
+        for i in range(len(lines[row])):
+            if start <= i < end:
+                char = lines[row + 1][i]
+                if char.isdigit():
+                    n = find_num_nbrs(lines[row + 1], visited, i, row + 1)
+                    if n != '':
+                        nbrs.append(n)
+
+    left = '.' if index <= 0 else lines[row][index - 1]
+    right = '.' if index >= len(lines[row]) - 1 else lines[row][index + 1]
+
+    if left.isdigit():
+        n = find_num_nbrs(lines[row], visited, index - 1, row)
+        if n != '':
+            nbrs.append(n)
     
+    if right.isdigit():
+        n = find_num_nbrs(lines[row], visited, index + 1, row)
+        if n != '':
+            nbrs.append(n)
+    
+    return nbrs
+
+
+
